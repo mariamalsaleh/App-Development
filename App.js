7404034ea77e31461a11fe0fca016733a7173c59
Mariam Al Saleh <mas21aam@gmail.com>
2021-02-17 18:18:50 +0400

    First practice

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-export default function App() {
+const { width, height } = Dimensions.get('window')
+export default App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={{ flexDirection: 'row' }}>
+
+        <View style={{ flex: 1, backgroundColor: 'white', height: 100, width: 100, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>This is the Header</Text>
+        </View>
+
+      </View>
+
+      <View style={{ flex:1, flexDirection: 'row', width: width, justifyContent: 'space-evenly', alignItems:'center' }}>
+        <View style={{  backgroundColor: 'pink', height: 100, width, width: 100, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Level 1</Text>
+        </View>
+        <View style={{  backgroundColor: 'blue', height: 100, width, width: 100, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Level 2</Text>
+        </View>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+
+        <View style={{ flex: 1, backgroundColor: 'orange', height: 100, width, width: 100, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Start</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: 'yellow', height: 100, width, width: 100,justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Options</Text>
+        </View>
+      </View>
+
+
+
     </View>
+
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, //% of screen you are in control of
+    flexDirection: 'column', //change orientation of default
+    backgroundColor: 'red',//color of selected content
+    alignItems: 'flex-start', //horizontal axis (row)
+    justifyContent: 'space-between', //default, vertical axis (column)
   },
 });
